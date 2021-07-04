f6d3e6677f04f8e05e39e609172096e29c810999
fujii52 <saaaaaaki52@gmail.com>
2021-07-04 23:53:04 +0900

    課題提出　制御文　
    やり直したやつです。

--- java study/1-4/Task1_4.java
+++ java study/1-4/Task1_4.java
@@ -15,6 +15,7 @@ public class Task1_4 {
     // ① 登録されている名前（USER_NAME）とパスワード（USER_PASSWORD）を定数で定義してください。
     private static final String USER_NAME = "saki";
     private static final String USER_PASSWORD = "saki52";
+    private static final String USER_PASSWORD = "alice123";
 
     // 定数（メッセージ）
     private static final String CONST_MSG_SUCCESS = "ログイン成功です。";
@@ -41,21 +42,27 @@ public class Task1_4 {
         //      定数を使用して「 ログイン成功です 」と出力してください。
         if (name.equals("alice") && pass.equals("alice123")){
         System.out.println(CONST_MSG_SUCCESS);
+        if (name.equals(USER_NAME) && pass.equals(USER_PASSWORD)){
+            System.out.println(CONST_MSG_SUCCESS);
+        }
 
         // ③ 「USER_RNAME」の値のみ等しい場合。
        //     定数を使用して「 パスワードに誤りがあります。 」 と出力してください。
-        } else if (!(pass.equals("alice123")) && name.equals("alice")){
-       System.out.println(CONST_MSG_ERROR_PASS);
+        else if (!(pass.equals(USER_PASSWORD) && name.equals(USER_NAME))){
+            System.out.println(CONST_MSG_ERROR_NAME);
+        }
 
         // ④ 「USER_PASSWORD」の値のみ等しい場合。
        //定数を使用して「 名前に誤りがあります。 」と出力してください。
-        } else if (!(name.equals("alice")) && pass.equals("alice123")){
-       System.out.println(CONST_MSG_ERROR_NAME);
+        else if (!(name.equals(USER_NAME) && pass.equals(USER_PASSWORD))){
+            System.out.println(CONST_MSG_ERROR_PASS);
+        }
 
         // ⑤ 「USER_NAME」も「USER_PASSWORD」の値も間違っていた場合。 
        //定数を使用して「 入力情報に誤りがあります。 」と出力してください。
-         } else { System.out.println(CONST_MSG_ERROR_INPUT);
-         }
+        else if (!(name.equals(USER_NAME) && (!(pass.equals(USER_PASSWORD))))){
+            System.out.println(CONST_MSG_ERROR_INPUT);
+        }
     }
 
 }
