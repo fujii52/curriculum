74cba128f04c2cf0c4fd8a414400f8f3f2966e68
fujii52 <saaaaaaki52@gmail.com>
2021-08-14 18:29:01 +0900

    check課題提出

---
+++ java study/Check2/src/check/Check.java
@@ -0,0 +1,38 @@
+package check;
+
+import constants.Constants;
+
+
+
+public class Check {
+	private String firstName = "fujii";
+	private String lastName = "saki";
+	
+	private void printName(String firstName ,String lastName) {
+				System.out.println("printNameメソッド→" + this.firstName + this.lastName);
+		    
+	}
+	
+
+	public static void main(String[] args) {
+		
+		
+		Check check = new Check();
+		
+			check.printName(check.firstName , check.lastName);
+		
+		 Pet pet = new Pet(Constants.CHECK_CLASS_JAVA , Constants.CHECK_CLASS_HOGE);
+		
+			pet.introduce();
+			
+		RobotPet robotPet = new  RobotPet(Constants.CHECK_CLASS_R2D2, Constants.CHECK_CLASS_LUKE);
+				
+			robotPet.introduce();
+				
+		
+
+		
+		}
+	
+
+}
